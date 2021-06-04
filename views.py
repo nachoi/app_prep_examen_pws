65adc43d10ef2b8538935bf5dbfe5eeb24a6b6fc
nachoi <49912403+nachoi@users.noreply.github.com>
2021-06-04 16:15:33 +0200

    Buscador con validación

--- views.py
+++ views.py
@@ -1,18 +1,23 @@
 from application import get_app
 from models import Company, get_db
-from flask import render_template, request
-from wtforms import StringField, Form
-from wtforms.validators import InputRequired, Length
+from flask import render_template, request, flash
+from wtforms import StringField
+from flask_wtf import Form, FlaskForm
+from wtforms.validators import InputRequired, Length, ValidationError, DataRequired
 
 app = get_app()
 db = get_db()
 
-class SearchForm(Form):
-    company = StringField('Company', validators=[InputRequired(),Length(min=1,max=4,message="1 a 4")],render_kw={"placeholder": "company symbol (1-4 letters)"})
+class SearchForm(FlaskForm):
+    company = StringField('Company', validators=[DataRequired(),Length(max=4)],render_kw={"placeholder": "company symbol (1-4 letters)"})
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     form = SearchForm(request.form)
-    if request.method == "GET":
-        return render_template('index.html',module='home', companies = Company.query.all(), form = form)
-    return render_template('index.html',module='home', companies = Company.query.all(), symbol=request.form["company"], form = form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            return render_template('index.html',module='home', companies = Company.query.all(), symbol=request.form["company"], form = form)
+        flash("The ticker must have between 1 and 4 letters")
+    return render_template('index.html',module='home', companies = Company.query.all(), form = form)
+

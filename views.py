cdfb5e1e88aa3d4388bbc2a786b9d806d3b22381
nachoi <49912403+nachoi@users.noreply.github.com>
2021-06-05 12:57:54 +0200

    Select with bbdd choices

--- views.py
+++ views.py
@@ -1,23 +1,28 @@
 from application import get_app
 from models import Company, get_db
 from flask import render_template, request, flash
-from wtforms import StringField
+from wtforms import SelectField
 from flask_wtf import Form, FlaskForm
-from wtforms.validators import InputRequired, Length, ValidationError, DataRequired
+from wtforms.validators import Length
 
 app = get_app()
 db = get_db()
 
-class SearchForm(FlaskForm):
-    company = StringField('Company', validators=[DataRequired(),Length(max=4)],render_kw={"placeholder": "company symbol (1-4 letters)"})
+
+class SelectForm(FlaskForm):
+    company = SelectField('Company', validators=[Length(min=1,max=4)], choices=[('', 'Select an option')])
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    form = SearchForm(request.form)
+    form = SelectForm()
+    companies = Company.query.all()
+    companies_list=[(i.ticker, i.ticker) for i in companies]
+    form.company.choices += companies_list
     if request.method == 'POST':
         if form.validate_on_submit():
             return render_template('index.html',module='home', companies = Company.query.all(), symbol=request.form["company"], form = form)
-        flash("The ticker must have between 1 and 4 letters")
-    return render_template('index.html',module='home', companies = Company.query.all(), form = form)
+        else:
+            flash("The ticker must have between 1 and 4 letters")
+    return render_template('index.html',module='home', companies = companies, form = form)
 
